958172fb91c7cf4e1d85097b286dcc603fd57a5a
Quyết Thành <thanh139ptit@gmail.com>
2025-05-01 02:11:33 +0700

    Clean folder

--- android/app/src/main/kotlin/com/mellivora24/landslide_fe/landslide_fe/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.mellivora24.landslide_fe.landslide_fe
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
--- lib/utils/router_names.dart
+++
@@ -1,4 +0,0 @@
-class RouteNames {
-  static const String homeScreen = '/home_screen';
-  static const String landingScreen = '/landing_screen';
-}
